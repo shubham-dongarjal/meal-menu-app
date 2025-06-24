49192d94047a32febef0dc96cf17a4f10f22a000
Shubham Dongarjal <shubham.dongarjal@arieotech.com>
2025-06-24 11:16:37 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'shubhamdongarjal15@gmail.com',      
-    password:             'ldbxwxqhsjypjtqe',    
+    user_name:            'youremail',      
+    password:             'securitypassword',    
     authentication:       'plain',
     enable_starttls_auto: true
   }
